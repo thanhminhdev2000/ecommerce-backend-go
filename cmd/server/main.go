5be1c5ea7d977191b5c527d639d3f207391217ee
Nguyen Thanh Minh <thanhminhspk05@gmail.com>
2025-01-09 19:23:05 +0700

    8. GIN vs Middleware

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,8 @@ import (
 	"github.com/thanhminhdev2000/ecommerce-backend-go/internal/routers"
 )
 
+
+
 func main() {
   r := routers.NewRouter()
   r.Run()
--- internal/routers/router.go
+++ internal/routers/router.go
@@ -1,12 +1,40 @@
 package routers
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	c "github.com/thanhminhdev2000/ecommerce-backend-go/internal/controller"
+	"github.com/thanhminhdev2000/ecommerce-backend-go/internal/middlewares"
 )
+
+func AA() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		fmt.Println("Before --> AA")
+		c.Next()
+		fmt.Println("After --> AA")
+	}
+}
+
+func BB() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		fmt.Println("Before --> BB")
+		c.Next()
+		fmt.Println("After --> BB")
+	}
+}
+
+func CC (c *gin.Context) {
+		fmt.Println("Before --> CC")
+		c.Next()
+		fmt.Println("After --> CC")
+	}
 
 func NewRouter() *gin.Engine {
 	r := gin.Default()
+
+	// use middleware
+	r.Use(middlewares.AuthenMiddleware(), AA(), BB(), CC)
 	v1 := r.Group("/v1")
 	{
 		v1.GET("/user/1", c.NewUserController().GetUserByID)
